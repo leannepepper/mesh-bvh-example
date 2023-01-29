2b40e0542a017627fa20762aee393a99add77e0e
Leanne Werner <leanne.werner.lw@gmail.com>
2023-01-28 21:51:13 -0800

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <CustomText />
+      {/* <CustomText /> */}
       <TextWithMask />
       <OrbitControls />
     </Canvas>
--- src/CustomText.tsx
+++ src/CustomText.tsx
@@ -4,72 +4,28 @@ import React, { useLayoutEffect, useEffect } from "react";
 import * as THREE from "three";
 import { Mesh, PlaneHelper } from "three";
 import { MeshBVH, MeshBVHVisualizer } from "three-mesh-bvh";
-import { CustomLineSegments } from "./CustomLineSegments";
 
 const font = "./fonts/pacifico/pacifico-regular-normal-400.json";
-
-const ClippingPlane = ({ bvh, model }: { bvh: MeshBVH; model: Mesh }) => {
-  const ref = React.useRef<THREE.Plane>();
-
-  useEffect(() => {
-    if (ref.current) {
-      const normal = new THREE.Vector3(0, -1, 0);
-      const constant = 0;
-      ref.current.normal.copy(normal);
-      ref.current.constant = constant;
-    }
-  }, []);
-
-  return null;
-};
 
 export const CustomText = () => {
   const ref = React.useRef<THREE.Mesh>();
   const localPlanes = [];
-  const { scene } = useThree();
+
   const [data, setData] = React.useState<{
     bvh: MeshBVH;
     model: Mesh;
   } | null>(null);
 
-  useLayoutEffect(() => {
-    if (ref.current) {
-      const bvh: MeshBVH = new MeshBVH(ref.current.geometry);
-      ref.current.geometry.boundsTree = bvh;
+  useEffect(() => {
+    if (!ref.current) return;
 
-      setData({ bvh, model: ref.current });
-
-      const visualizer = new MeshBVHVisualizer(ref.current, 10);
-      //   scene.add(visualizer);
-
-      const testPlane = new THREE.Plane().setFromCoplanarPoints(
-        new THREE.Vector3(
-          bvh.geometry.attributes.position.array[0],
-          bvh.geometry.attributes.position.array[1],
-          bvh.geometry.attributes.position.array[2]
-        ),
-        new THREE.Vector3(
-          bvh.geometry.attributes.position.array[0],
-          bvh.geometry.attributes.position.array[1],
-          bvh.geometry.attributes.position.array[2]
-        ),
-        new THREE.Vector3(
-          bvh.geometry.attributes.position.array[0],
-          bvh.geometry.attributes.position.array[1],
-          bvh.geometry.attributes.position.array[2]
-        )
-      );
-
-      const planeHelper = new PlaneHelper(testPlane, 1, 0xff0000);
-      scene.add(planeHelper);
-
-      localPlanes.push(testPlane);
-    }
-  }, []);
+    const bvh = new MeshBVH(ref.current.geometry);
+    setData({ bvh, model: ref.current });
+  }, [ref]);
 
   return (
     <>
-      {/* <Text3D
+      <Text3D
         ref={ref}
         font={font}
         scale={1}
@@ -85,8 +41,7 @@ export const CustomText = () => {
           side={THREE.DoubleSide}
           clippingPlanes={localPlanes}
         ></meshPhongMaterial>
-      </Text3D> */}
-      {/* {data && <CustomLineSegments {...data} />} */}
+      </Text3D>
     </>
   );
 };
