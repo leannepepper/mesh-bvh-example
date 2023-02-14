619f930041b0acfb1a84ebd5ea8a9a623abd2938
Leanne Werner <leanne.werner.lw@gmail.com>
2023-02-13 23:09:34 -0800

    fixing morphTarget error

--- src/MorphTargets.tsx
+++ src/MorphTargets.tsx
@@ -8,42 +8,42 @@ export default function MorphTargets() {
   const geometryRef = new THREE.BoxGeometry();
   const { scene } = useThree() as any;
 
+  const positionAttribute = geometryRef.attributes.position;
+  geometryRef.morphTargetsRelative = true;
+
+  const spherePositions = [];
+
+  for (let i = 0; i < positionAttribute.count; i++) {
+    const x = positionAttribute.getX(i);
+    const y = positionAttribute.getY(i);
+    const z = positionAttribute.getZ(i);
+
+    spherePositions.push(
+      x * Math.sqrt(1 - (y * y) / 2 - (z * z) / 2 + (y * y * z * z) / 3),
+      y * Math.sqrt(1 - (z * z) / 2 - (x * x) / 2 + (z * z * x * x) / 3),
+      z * Math.sqrt(1 - (x * x) / 2 - (y * y) / 2 + (x * x * y * y) / 3)
+    );
+  }
+
+  geometryRef.morphAttributes.position = [];
+
+  geometryRef.morphAttributes.position[0] = new THREE.Float32BufferAttribute(
+    spherePositions,
+    3
+  );
+
+  useFrame((state, delta) => {
+    // if the morphTargetInfluences is greater than 1, then set it to 0
+    if (ref.current.morphTargetInfluences[0] > 1) {
+      ref.current.morphTargetInfluences[0] = 0;
+    } else {
+      ref.current.morphTargetInfluences[0] += 0.01;
+    }
+  });
+
   useLayoutEffect(() => {
     ref.current.updateMorphTargets();
   }, []);
-
-  useEffect(() => {
-    const positionAttribute = geometryRef.attributes.position;
-
-    const spherePositions = [];
-
-    for (let i = 0; i < positionAttribute.count; i++) {
-      const x = positionAttribute.getX(i);
-      const y = positionAttribute.getY(i);
-      const z = positionAttribute.getZ(i);
-
-      spherePositions.push(
-        x * Math.sqrt(1 - (y * y) / 2 - (z * z) / 2 + (y * y * z * z) / 3),
-        y * Math.sqrt(1 - (z * z) / 2 - (x * x) / 2 + (z * z * x * x) / 3),
-        z * Math.sqrt(1 - (x * x) / 2 - (y * y) / 2 + (x * x * y * y) / 3)
-      );
-    }
-
-    geometryRef.morphAttributes.position = [];
-
-    ref.current.updateMorphTargets();
-
-    geometryRef.morphAttributes.position[0] = new THREE.Float32BufferAttribute(
-      spherePositions,
-      3
-    );
-
-    ref.current.morphTargetInfluences[0] = 0.5;
-  }, []);
-
-  useFrame(() => {
-    ref.current.morphTargetInfluences[0] += 0.01;
-  });
 
   return (
     <>
