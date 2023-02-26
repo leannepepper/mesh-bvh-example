a518cb4d3da7e59d897ee0ae41c2b42b75347be3
Leanne Werner <leanne.werner.lw@gmail.com>
2023-02-25 20:48:19 -0800

    morphtargets by group

--- src/MorphTargetsPath.tsx
+++ src/MorphTargetsPath.tsx
@@ -5,104 +5,70 @@ import { MeshPhongMaterial } from "three";
 
 export default function MorphTargetsPath() {
   const ref = useRef(null);
-  const segments = 100;
+  const segments = 10;
   const geometry = new THREE.TorusKnotGeometry(2, 0.2, segments, 8, 1, 5);
 
-  //geometry.morphTargetsRelative = true;
-  /** Clear all the groups and make two groups for each face of the cube */
+  //   geometry.morphTargetsRelative = true;
   geometry.clearGroups();
 
-  //   geometry.addGroup(3, 3, 1);
-  //   geometry.addGroup(6, 3, 2);
-  //   geometry.addGroup(9, 3, 3);
-  //   geometry.addGroup(12, 3, 4);
-  //   geometry.addGroup(15, 3, 5);
-  //   geometry.addGroup(18, 3, 6);
-  //   geometry.addGroup(21, 3, 7);
-  //   geometry.addGroup(24, 3, 8);
-  //   geometry.addGroup(27, 3, 9);
-  //   geometry.addGroup(30, 3, 10);
-  //   geometry.addGroup(33, 3, 11);
-
   let materialIndex = 0;
-  for (
-    let i = 0;
-    i < geometry.attributes.position.array.length * 2;
-    i = i + 48
-  ) {
+  for (let i = 0; i < geometry.attributes.position.array.length; i += 48) {
     geometry.addGroup(i, 48, materialIndex);
     materialIndex++;
   }
   console.log({ geometry });
 
-  /** Create the MorphTarget for the sphere position */
-  //   const positionAttribute = geometry.attributes.position;
-  //   const indecesAttribute = geometry.attributes;
+  /** Create the MorphTarget for start positions */
+  const positionAttribute = geometry.attributes.position;
+  const group1Positions = [];
 
-  //   const group1Positions = [];
-  //   const group2Positions = [];
+  // for the first group in the geometry, create a morph target that goes to the last set of vertices in the group
+  const group1 = geometry.groups[0];
 
-  //   for (let i = 0; i < positionAttribute.count; i = i + 3) {
-  //     const x = positionAttribute.getX(i);
-  //     const y = positionAttribute.getY(i);
-  //     const z = positionAttribute.getZ(i);
+  // loop through all the groups
+  for (let i = 0; i < geometry.groups.length; i++) {
+    const group = geometry.groups[i];
+    // loop through all the vertices in the group
+    for (let j = group.start; j < group.start + group.count; j++) {
+      // get the x, y, and z values for the vertex
+      const x = positionAttribute.getX(j);
+      const y = positionAttribute.getY(j);
+      const z = positionAttribute.getZ(j);
+      // add the x, y, and z values to the morph target array
+      group1Positions.push(x, y, z);
+    }
 
-  //     const x2 = positionAttribute.getX(i + 1);
-  //     const y2 = positionAttribute.getY(i + 1);
-  //     const z2 = positionAttribute.getZ(i + 1);
+    // if this is the first group, add the morph target to the geometry
+    if (i === 0) {
+      geometry.morphAttributes.position = [];
+      geometry.morphAttributes.position[0] = new THREE.Float32BufferAttribute(
+        group1Positions,
+        3
+      );
+    }
+  }
 
-  //     const x3 = positionAttribute.getX(i + 2);
-  //     const y3 = positionAttribute.getY(i + 2);
-  //     const z3 = positionAttribute.getZ(i + 2);
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.morphTargetInfluences[0] = Math.abs(
+        Math.sin(state.clock.elapsedTime)
+      );
+    }
+  });
 
-  //     if (i > 3) {
-  //       group1Positions.push(x, y, z, x2, y2, z2, x3, y3, z3);
-  //     } else {
-  //       group1Positions.push(
-  //         x * 0.002,
-  //         y * 0.002,
-  //         z * 0.002,
-  //         x2 * 0.002,
-  //         y2 * 0.002,
-  //         z2 * 0.002,
-  //         x3 * 0.002,
-  //         y3 * 0.002,
-  //         z3 * 0.002
-  //       );
-  //     }
-  //   }
+  useLayoutEffect(() => {
+    ref.current.updateMorphTargets();
+    ref.current.morphTargetInfluences = [];
+  }, []);
 
-  //   geometry.morphAttributes.position = [];
-  //   geometry.morphAttributes.position[0] = new THREE.Float32BufferAttribute(
-  //     group1Positions,
-  //     3
-  //   );
-
-  // geometryRef.morphAttributes.position[1] = new THREE.Float32BufferAttribute(
-  //   group2Positions,
-  //   3
-  // );
-
-  //   useFrame((state, delta) => {
-  //     if (ref.current) {
-  //       ref.current.morphTargetInfluences[0] = Math.abs(
-  //         Math.sin(state.clock.elapsedTime)
-  //       );
-  //     }
-  //   });
-
-  //   useLayoutEffect(() => {
-  //     ref.current.updateMorphTargets();
-  //     ref.current.morphTargetInfluences = [];
-  //   }, []);
-
-  // make an array of 50 meshPhong materials of different colors
+  // make an array of meshPhong materials of different colors
   const colors = [];
   for (let i = 0; i < segments; i++) {
     colors.push(
       new MeshPhongMaterial({
         color: Math.random() * 0xffffff,
-        wireframe: false,
+        wireframe: true,
+        visible: true,
       })
     );
   }
