6313f0424185b69b49e1be61694282b3494f4412
Leanne Werner <leanne.werner.lw@gmail.com>
2023-05-07 14:35:08 -0700

    adding blue offset

--- src/CircleArt.tsx
+++ src/CircleArt.tsx
@@ -12,18 +12,29 @@ interface BloomProps {
 }
 
 export default function CircleArt() {
+  const bloomProps: BloomProps = {
+    blendFunction: 0,
+    luminanceThreshold: 0.1,
+    height: 200,
+    intensity: 1.0,
+  };
+
+  return (
+    <>
+      <PinkCircles />
+      <BlueCircles />
+      <EffectComposer>
+        <Bloom {...bloomProps} />
+      </EffectComposer>
+    </>
+  );
+}
+
+function PinkCircles() {
   const ref = useRef(null);
   const count = 6;
   const angle = (Math.PI * 2) / count;
   const radius = 0.05;
-
-  const bloomProps: BloomProps = {
-    blendFunction: 0,
-    luminanceSmoothing: 0.025,
-    luminanceThreshold: 0.1,
-    height: 100,
-    intensity: 0.7,
-  };
 
   useLayoutEffect(() => {
     for (let i = 0; i < count; i++) {
@@ -35,25 +46,57 @@ export default function CircleArt() {
           Math.sin(angle * i) * 0.3
         )
       );
-      console.log(ref.current);
     }
   }, []);
+
   useFrame(() => {
-    //ref.current.rotation.y += 0.03;
+    ref.current.rotation.y += 0.01;
   });
+
   return (
-    <>
-      <instancedMesh ref={ref} args={[null, null, count]}>
-        <sphereGeometry args={[radius]} />
-        <meshStandardMaterial
-          attach="material"
-          color="deeppink"
-          side={THREE.DoubleSide}
-        />
-      </instancedMesh>
-      <EffectComposer>
-        <Bloom {...bloomProps} />
-      </EffectComposer>
-    </>
+    <instancedMesh ref={ref} args={[null, null, count]}>
+      <sphereGeometry args={[radius]} />
+      <meshStandardMaterial
+        attach="material"
+        color="deeppink"
+        side={THREE.DoubleSide}
+      />
+    </instancedMesh>
+  );
+}
+
+function BlueCircles() {
+  const ref = useRef(null);
+  const count = 6;
+  const radius = 0.05;
+  const offset = 0.1;
+  const angle = (Math.PI * 2) / count + offset;
+
+  useLayoutEffect(() => {
+    for (let i = 0; i < count; i++) {
+      ref.current.setMatrixAt(
+        i,
+        new THREE.Matrix4().makeTranslation(
+          Math.cos(angle * i) * 0.3,
+          0,
+          Math.sin(angle * i) * 0.3
+        )
+      );
+    }
+  }, []);
+
+  useFrame(() => {
+    ref.current.rotation.y -= 0.01;
+  });
+
+  return (
+    <instancedMesh ref={ref} args={[null, null, count]}>
+      <sphereGeometry args={[radius]} />
+      <meshStandardMaterial
+        attach="material"
+        color="lightblue"
+        side={THREE.DoubleSide}
+      />
+    </instancedMesh>
   );
 }
